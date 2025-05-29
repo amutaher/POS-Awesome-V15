d3787b825689ecb35df66536199859739b258f0f
Defendicon <robin001100110@gmail.com>
2025-05-29 22:35:34 +0500

    feat: Integrate service worker registration in posapp.js for enhanced PWA support. This addition improves offline capabilities by registering the service worker immediately, ensuring better resource management and user experience during connectivity issues.

--- posawesome/public/js/posapp/posapp.js
+++ posawesome/public/js/posapp/posapp.js
@@ -1,3 +1,5 @@
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 import { createVuetify } from 'vuetify';
 import { createApp } from 'vue';
 import eventBus from './bus';
