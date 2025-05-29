39ea1a911cf56072e602e42f3137eea6f878ce09
Defendicon <robin001100110@gmail.com>
2025-05-29 14:26:33 +0500

    feat: Update asset paths and enhance webpack configuration in posawesome module. Refactored CSS and JS inclusion to utilize the new directory structure, improving organization and maintainability. Added Vue and MiniCssExtract plugins for better handling of Vue components and CSS extraction, optimizing the build process for production.

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -17,8 +17,12 @@ app_license = "GPLv3"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/posawesome/css/posawesome.css"
 # app_include_js = "/assets/posawesome/js/posawesome.js"
+app_include_css = [
+    "/assets/posawesome/dist/css/posawesome.min.css"
+]
+
 app_include_js = [
-    "/assets/js/posawesome.min.js"
+    "/assets/posawesome/dist/js/posawesome.min.js"
 ]
 
 # include js, css files in header of web template
--- posawesome/webpack.config.js
+++ posawesome/webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
     entry: {
-        'posawesome': './posawesome/public/js/posapp/Home.vue',
-        'posawesome-vue': './posawesome/public/js/posawesome-vue.js'
+        posawesome: './posawesome/public/js/posapp/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'posawesome/public/dist'),
-        filename: '[name].min.js',
-        publicPath: '/assets/'
+        filename: 'js/[name].min.js',
+        publicPath: '/assets/posawesome/dist/'
     },
     module: {
         rules: [
@@ -21,25 +22,39 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env']
+                }
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
             }
         ]
     },
     resolve: {
         extensions: ['.js', '.vue'],
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.resolve(__dirname, 'posawesome/public/js/posapp')
         }
     },
     plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css'
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: '"production"'
             }
         })
-    ]
+    ],
+    optimization: {
+        minimize: true
+    }
 }; 
